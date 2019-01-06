d370d2b87d1f0c8d606183b3c8a5ad2bcace71af
Didac6 <akleonic@gmail.com>
2019-01-06 19:37:51 +0100

    Jugabilidad nivel 1

--- inter.py
+++ inter.py
@@ -1,10 +1,32 @@
 import tkinter as tk                # python 3
 from tkinter import font  as tkfont # python 3
 from PIL import Image, ImageTk
-
+from tkinter.messagebox import showinfo
+from functools import partial
 #import Tkinter as tk     # python 2
 #import tkFont as tkfont  # python 2
+"""extra functions"""
+def find_all_paths(graph, start, end, path=[]):
+      path = path + [start]
+      if start == end:
+          return [path]
+      if not start in graph:
+          return []
+      paths = []
+      for node in graph[start]:
+          if node not in path:
+              newpaths = find_all_paths(graph, node, end, path)
+              for newpath in newpaths:
+                  paths.append(newpath)
+      return paths
 
+def sorthestPath(graph, start, end):
+    caminos = find_all_paths(graph, start, end)
+    longs = [len(p) for p in caminos]
+    short = min(longs)
+    return [a for a in caminos if len(a) == short]
+    
+"""interfaz"""
 class SampleApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -71,7 +93,8 @@ class StartPage(tk.Frame):
         # button2.pack()
 
 class PlayPage(tk.Frame):
-
+    
+    
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
@@ -83,7 +106,7 @@ class PlayPage(tk.Frame):
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
         
-        im = Image.open("background 1.png")
+        im = Image.open("nivel1.png")
         background_image = ImageTk.PhotoImage(im)
         background_label = tk.Label(self, image=background_image)
         background_label.image = background_image
@@ -91,23 +114,64 @@ class PlayPage(tk.Frame):
         self.crearNodos()
         print(self.adyacencias)
         
-        
-        
-
+        self.survivor = -1#posicion del superviviente
+        self.zombi = [4]# posicion del/de los zombie/s
+        for z in self.zombi:
+            self.nodos[z].configure(image = self.imZombie)
+            
     def crearNodos(self):
-        self.adyacencias = []
+        self.imNode = ImageTk.PhotoImage(Image.open("nodo.png"))
+        self.imZombie = ImageTk.PhotoImage(Image.open("zombie.png"))
+        self.imSurvivor = ImageTk.PhotoImage(Image.open("superv.png"))
+        
+        self.adyacencias = {} # el grafo es un diccionario
         self.nodos = []
         nodes = [line.rstrip('\n') for line in open('nivel1.txt')]
+        j = 0
         for l in nodes:
-            pos, ad = l.split(";")
-            pos = pos.split(",")
-            self.adyacencias.append([int(i) for i in ad.split(",")]) 
-            im = Image.open("nodo.png")
-            bu = ImageTk.PhotoImage(im)
-            self.nodos.append(tk.Button(self, command = lambda: print(""), image = bu))
-            self.nodos[-1].image = bu
-            self.nodos[-1].place(x = int(pos[0]), y = 80 + int(pos[1]))
+            posi, ad = l.split(";")
+            pos = {j:[int(i) for i in posi.split(",")]}
+            self.adyacencias[j] = [int(i) for i in ad.split(",")]
+            self.nodos.append(tk.Button(self, command = partial(self.mover, j) , image = self.imNode))
+            self.nodos[-1].image = self.imNode
+            self.nodos[-1].place(x = pos[j][0], y = 80 + pos[j][1])
+            j += 1
+            
+    def mover(self, pos):    
+        print(pos)
+        if pos not in self.zombi:# no puedes colocarte en un zombie
+            if self.survivor == -1 or pos in self.adyacencias[self.survivor]:#colocate o muevete a una adyacente
+                if self.survivor != -1:
+                    self.nodos[self.survivor].configure(image = self.imNode)
+                self.survivor = pos
+                self.nodos[self.survivor].configure(image = self.imSurvivor)
+                z = 0
+                while z < len(self.zombi):
+                    sorth = sorthestPath(self.adyacencias, self.zombi[z], self.survivor) 
+                    l, j = len(sorth), 0
+                    moved = False
+                    while j < l and not moved:
+                        newpos = sorth[j][1]
+                        print(newpos)
+                        if newpos not in self.zombi:# la casilla no esta ocupada ya por otro zombie
+                            self.nodos[self.zombi[z]].configure(image = self.imNode)
+                            self.nodos[newpos].configure(image = self.imZombie)
+                            self.zombi[z] = newpos
+                            moved = True
+                    z += 1
+                    
+            else:
+               showinfo("Message", "Move only to an adyacent node") 
+               return
+        else:
+           showinfo("Message", "You can't select a zombie node") 
+           return
+       
+        if self.survivor in self.zombi:
+            self.survivor = -1
+            showinfo("Message", "Your survivor has died") 
         
+    
         
         
 class RulePage(tk.Frame):
