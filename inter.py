4bc828a74a454aab5cf237db1b84944d2cbd3767
Didac6 <akleonic@gmail.com>
2019-01-07 20:10:14 +0100

    Añadido niveles y menu de seleccion de niveles

--- inter.py
+++ inter.py
@@ -42,17 +42,20 @@ class SampleApp(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+        niveles = [[1,"nivel1.png","nivel1.txt",[2]],[2,"nivel2.png","nivel2.txt",[5,7]],[3,"nivel3.png","nivel3.txt", [15]]]
         self.frames = {}
-        for F in (StartPage, PlayPage, RulePage):
+        for F in (StartPage,LevelSelectPage, RulePage):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
-
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
-
+            
+        for nv in niveles:
+            page_name = "PlayPage" + str(nv[0])
+            frame = PlayPage(parent=container, controller=self, imagen = nv[1], datos = nv[2], posZombies = nv[3])
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+            
         self.show_frame("StartPage")
 
     def show_frame(self, page_name):
@@ -76,7 +79,7 @@ class StartPage(tk.Frame):
 
         label.pack(side="top", fill="x", pady=10)
 
-        playButton = tk.Button(self, bg="PaleGreen1", text="JUGAR",command=lambda: controller.show_frame("PlayPage"))
+        playButton = tk.Button(self, bg="PaleGreen1", text="JUGAR",command=lambda: controller.show_frame("LevelSelectPage"))
         # justify="center"
         ruleButton = tk.Button(self, bg="light blue", text="REGLAS",command=lambda: controller.show_frame("RulePage"))
         # , justify="center"
@@ -92,10 +95,32 @@ class StartPage(tk.Frame):
         # button1.pack()
         # button2.pack()
 
+class LevelSelectPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        im1 = ImageTk.PhotoImage(Image.open("nivel1.png").resize((400,130))) 
+        im2 = ImageTk.PhotoImage(Image.open("nivel2.png").resize((400,130))) 
+        im3 = ImageTk.PhotoImage(Image.open("nivel3.png").resize((400,130)))  
+        
+        niv1 = tk.Button(self, image = im1, command=lambda: controller.show_frame("PlayPage1"))
+        niv1.image = im1
+        niv2 = tk.Button(self, image = im2, command=lambda: controller.show_frame("PlayPage2"))
+        niv2.image = im2
+        niv3 = tk.Button(self, image = im3, command=lambda: controller.show_frame("PlayPage3"))
+        niv3.image = im3
+
+        niv1.pack(side="top", expand="True")
+        niv2.pack(side="top", expand="True")
+        niv3.pack(side="top", expand="True")
+
+        
 class PlayPage(tk.Frame):
     
     
-    def __init__(self, parent, controller):
+    def __init__(self, parent, controller, imagen, datos, posZombies):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         
@@ -106,27 +131,26 @@ class PlayPage(tk.Frame):
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
         
-        im = Image.open("nivel1.png")
-        background_image = ImageTk.PhotoImage(im)
+        background_image = ImageTk.PhotoImage(Image.open(imagen))
         background_label = tk.Label(self, image=background_image)
         background_label.image = background_image
         background_label.pack()
-        self.crearNodos()
-        print(self.adyacencias)
+        self.crearNodos(datos)
         
         self.survivor = -1#posicion del superviviente
-        self.zombi = [4]# posicion del/de los zombie/s
+        self.zombi = posZombies# posicion del/de los zombie/s
         for z in self.zombi:
             self.nodos[z].configure(image = self.imZombie)
             
-    def crearNodos(self):
+    def crearNodos(self, datos):
         self.imNode = ImageTk.PhotoImage(Image.open("nodo.png"))
         self.imZombie = ImageTk.PhotoImage(Image.open("zombie.png"))
         self.imSurvivor = ImageTk.PhotoImage(Image.open("superv.png"))
         
         self.adyacencias = {} # el grafo es un diccionario
+        
         self.nodos = []
-        nodes = [line.rstrip('\n') for line in open('nivel1.txt')]
+        nodes = [line.rstrip('\n') for line in open(datos)]
         j = 0
         for l in nodes:
             posi, ad = l.split(";")
@@ -136,9 +160,8 @@ class PlayPage(tk.Frame):
             self.nodos[-1].image = self.imNode
             self.nodos[-1].place(x = pos[j][0], y = 80 + pos[j][1])
             j += 1
-            
-    def mover(self, pos):    
-        print(pos)
+        
+    def mover(self, pos):
         if pos not in self.zombi:# no puedes colocarte en un zombie
             if self.survivor == -1 or pos in self.adyacencias[self.survivor]:#colocate o muevete a una adyacente
                 if self.survivor != -1:
@@ -150,15 +173,25 @@ class PlayPage(tk.Frame):
                     sorth = sorthestPath(self.adyacencias, self.zombi[z], self.survivor) 
                     l, j = len(sorth), 0
                     moved = False
-                    while j < l and not moved:
+                    while j < l and not moved and sorth != []:
                         newpos = sorth[j][1]
-                        print(newpos)
                         if newpos not in self.zombi:# la casilla no esta ocupada ya por otro zombie
                             self.nodos[self.zombi[z]].configure(image = self.imNode)
                             self.nodos[newpos].configure(image = self.imZombie)
                             self.zombi[z] = newpos
                             moved = True
+                        
+                        j += 1
+                        
                     z += 1
+                
+                #comprobar si el superviviente esta bloqueado
+                blocked = 0
+                for i in self.adyacencias[self.survivor]:
+                    blocked += 1 if i in self.zombi else 0
+                    
+                if blocked == len(self.adyacencias[self.survivor]):
+                    showinfo("Message", "The Survivor is trapped by zombies... ") 
                     
             else:
                showinfo("Message", "Move only to an adyacent node") 
